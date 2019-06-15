3db286b38f13217980ca4975c5de54a249bba7e1
llcernicchiaro <lopescernicchiaro@gmail.com>
2019-06-15 14:12:25 -0300

    Updates

---
+++ precache-manifest.02c0c47053bbe90fa5cf32a19ca04635.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d7e4b9844851b73d92e760f50372b22c",
+    "url": "/my-app/index.html"
+  },
+  {
+    "revision": "c6beca148f3ae15262b7",
+    "url": "/my-app/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/my-app/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "c6beca148f3ae15262b7",
+    "url": "/my-app/static/js/main.7c879df4.chunk.js"
+  },
+  {
+    "revision": "9930044c31793394defe",
+    "url": "/my-app/static/js/runtime~main.6019dd84.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/my-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
